b180d778a03171a87394ad4aab62eceb0d1575df
Steve Behm <stevtbehm1@gmail.com>
2022-03-02 18:58:28 +0000

    Create an addEventListener to the NEW link that will render a new image on the entry page.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var $tryItBtn = document.querySelector('.try-it-btn');
 var $newStoryPage = document.querySelector('.new-story');
 var $landingPage = document.querySelector('.first-page');
 var $newLink = document.querySelector('.new-link');
+var $randomImg = document.querySelector('.random-art');
 
 /* Function to change view to new stiory if user clicks try it */
 function handleClickRandomImage(event) {
@@ -14,7 +15,40 @@ function handleClickRandomImage(event) {
 function handleNewClick(event) {
   $landingPage.className = 'first-page container hidden';
   $newStoryPage.className = 'container new-story';
+  // below will get a new img if the user clicks on the new link
+  getRandomArtObject();
 }
 
 $tryItBtn.addEventListener('click', handleClickRandomImage);
 $newLink.addEventListener('click', handleNewClick);
+
+// Retrieving list of objects from the API
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=art');
+xhr.responseType = 'json';
+
+function getRandomArtObject() {
+  // below produces a random string version of an objectId that has an image value
+  var objectIdArr = xhr.response.objectIDs;
+  var randomObjectId = objectIdArr[Math.floor(Math.random() * objectIdArr.length)];
+  var randomObjectIdString = randomObjectId.toString();
+
+  // This API request is using the string version of the objectID to retrieve the actual
+  // data on the specific object.
+  // it is then going to take the primary img url and put it into the src value for
+  // the random image when creating a story.
+  var objectData = new XMLHttpRequest();
+  objectData.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + randomObjectIdString);
+  objectData.responseType = 'json';
+
+  function getRandomArtImg() {
+    var imgURL = objectData.response.primaryImage;
+    $randomImg.setAttribute('src', imgURL);
+  }
+
+  objectData.addEventListener('load', getRandomArtImg);
+  objectData.send();
+}
+
+xhr.addEventListener('load', getRandomArtObject);
+xhr.send();
