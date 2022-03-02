ae9018d5f9d976ded7e6342185b328f6053a8014
Steve Behm <stevtbehm1@gmail.com>
2022-03-02 21:29:20 +0000

    made two suggested changes for function to retrieve API data.

--- js/main.js
+++ js/main.js
@@ -20,39 +20,42 @@ function handleNewClick(event) {
   getRandomArtObject();
 }
 
-// Retrieving list of objects from the API
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=art');
-xhr.responseType = 'json';
-
 function getRandomArtObject() {
-  // below produces a random string version of an objectId that has an image value
-  var objectIdArr = xhr.response.objectIDs;
-  var randomObjectId = objectIdArr[Math.floor(Math.random() * objectIdArr.length)];
-  var randomObjectIdString = randomObjectId.toString();
+  // Retrieving list of objects from the API
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=art');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // below produces a random string version of an objectId that has an image value
+    var objectIdArr = xhr.response.objectIDs;
+    var randomObjectId = objectIdArr[Math.floor(Math.random() * objectIdArr.length)];
+    var randomObjectIdString = randomObjectId.toString();
 
-  // This API request is using the string version of the objectID to retrieve the actual
-  // data on the specific object.
-  // it is then going to take the primary img url and put it into the src value for
-  // the random image when creating a story.
-  var objectData = new XMLHttpRequest();
-  objectData.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + randomObjectIdString);
-  objectData.responseType = 'json';
+    // This API request is using the string version of the objectID to retrieve the actual
+    // data on the specific object.
+    // it is then going to take the primary img url and put it into the src value for
+    // the random image when creating a story.
+    var objectData = new XMLHttpRequest();
+    objectData.open('GET', 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + randomObjectIdString);
+    objectData.responseType = 'json';
+    // when the page loads this add event listener method will be called
+    // which will in turn run the function to call the getRandomImg function
+    // the getRandomArtImg function will then change the value or the src attr
+    // to the value of objectData.response.primaryImage
+    objectData.addEventListener('load', function () {
+      getRandomArtImg(objectData.response.primaryImage);
+    });
+    objectData.send();
+  });
+  // sends the request to the API
+  xhr.send();
+}
 
-  function getRandomArtImg() {
-    var imgURL = objectData.response.primaryImage;
-    $randomImg.setAttribute('src', imgURL);
-  }
-
-  objectData.addEventListener('load', getRandomArtImg);
-  objectData.send();
+function getRandomArtImg(url) {
+  $randomImg.setAttribute('src', url);
 }
 
 // when user clicks the try it button it swaps views and produces random image
 $tryItBtn.addEventListener('click', handleClickRandomImage);
 // when user clicks the new button it swaps views and gets a new image
 $newLink.addEventListener('click', handleNewClick);
-// listens for a load event to make a request to the API
-xhr.addEventListener('load', getRandomArtObject);
-// sends the equest to the API
-xhr.send();
