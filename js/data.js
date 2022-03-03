2724f324ddbc482fe463b47343b7ee2920d875db
Steve Behm <stevtbehm1@gmail.com>
2022-03-02 17:01:20 -0800

    Verify that each new entry receives an incrementing 'entryId'.

--- js/data.js
+++ js/data.js
@@ -6,12 +6,18 @@ var data = {
   nextStoryId: 1
 };
 
+// here we are gathering local storage from the previous visit or
+// prior to the last refresh
 var previousStoriesJSON = localStorage.getItem('local-storage');
 
+// if the local storage was not empty then we update the data model
+// with the previous data the user was using
 if (previousStoriesJSON !== null) {
   data = JSON.parse(previousStoriesJSON);
 }
 
+// if it was empty then we stringify the data into a JSON string
+// that can be storage before the user refreshes or loses the page
 function handleBeforeUnload(event) {
   var localStorageData = JSON.stringify(data);
   localStorage.setItem('local-storage', localStorageData);
